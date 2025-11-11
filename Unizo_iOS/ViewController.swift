7cbadd95367487456c7668f418b302b161b6ae9d
Somesh Das <someshdasbb07@gmail.com>
2025-11-11 11:56:30 +0530

    Initial Commit

---
+++ Unizo_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Unizo_iOS
+//
+//  Created by Somesh on 11/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Unizo_iOSTests/Unizo_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Unizo_iOSTests.swift
+//  Unizo_iOSTests
+//
+//  Created by Somesh on 11/11/25.
+//
+
+import Testing
+@testable import Unizo_iOS
+
+struct Unizo_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
