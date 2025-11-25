83f167b1fa53e588d2a170269901e294c50336c7
SohamB4746Y <sohambhattacharya023@gmail.com>
2025-11-26 04:02:02 +0530

    commit on 25 night

---
+++ Unizo_iOS/Controllers/ChatDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ChatDetailViewController.swift
+//  Unizo_iOS
+//
+//  Created by Soham Bhattacharya on 26/11/25.
+//
+
+import UIKit
+
+class ChatDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
