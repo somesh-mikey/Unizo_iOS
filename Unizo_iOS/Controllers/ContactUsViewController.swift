886fe6de546d4180568891126058c3780b4daacb
SohamB4746Y <sohambhattacharya023@gmail.com>
2026-02-17 08:36:37 +0530

    tab bar settings screen fixed & carousel fix

--- Unizo_iOS/Controllers/ContactUsViewController.swift
+++ Unizo_iOS/Controllers/ContactUsViewController.swift
@@ -45,6 +45,16 @@ class ContactUsViewController: UIViewController {
         setupKeyboardHandling()
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tabBarController?.tabBar.isHidden = true
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.tabBarController?.tabBar.isHidden = false
+    }
+
     // MARK: - Keyboard Handling
     private func setupKeyboardHandling() {
         NotificationCenter.default.addObserver(
